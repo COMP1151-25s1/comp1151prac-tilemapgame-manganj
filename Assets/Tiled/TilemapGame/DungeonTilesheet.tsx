985701d49c8baa66f03218822c853da3ef9b3fed
manganj <joshua.mangan1@students.mq.edu.au>
2025-05-29 09:05:38 +1000

    Physics

--- Assets/Tiled/TilemapGame/DungeonTilesheet.tsx
+++ Assets/Tiled/TilemapGame/DungeonTilesheet.tsx
@@ -233,6 +233,7 @@
  <tile id="66">
   <objectgroup draworder="index" id="2">
    <object id="1" x="1" y="0" width="14" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="73">
